08bba1616b5218d69315b7a09efcb674da5c98f2
MarioGG61 <mgomezg61@educarex.es>
2023-11-10 21:26:39 +0100

    Casi acabada la pagina del gatete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ContenidoComponent } from "./contenido/contenido.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, ContenidoComponent]
 })
 export class AppComponent {
   title = 'proyecto1';
